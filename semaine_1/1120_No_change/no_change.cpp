c629030875f5add6c0f4fb4bec4b6d6f8186653a
Mickaël Seznec <mickael.seznec@gmail.com>
2015-05-08 12:40:07 +0200

    No change should work, add recursivity

--- semaine_1/1120_No_change/no_change.cpp
+++ semaine_1/1120_No_change/no_change.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void adem_algo (int amount, int *coin_value, int coins) {
-    int coin_used[5] = {0,};
-    bool solved = false;
+int cases, amount, coins;
+int coin_value[5];
+int coin_used[5];
 
-    while (amount >= coin_value[0]) {
-        amount -= coin_value[0];
-        coin_used[0]++;
-    }
+inline int coin_max (int index, int max_coin, int amount) {
 
-    for (int i = 1; i < coins; i++) {
-        while (amount >= coin_value[i] && coin_used[i] < coin_used[i-1]) {
-            amount -= coin_value[i];
-            coin_used[i]++;
-        }
-        if (amount == 0) {
-            solved = true;
-            break;
-        }
-    }
-    if (solved)
-        cout << "YES" << endl << endl;
-    else
-        cout << "NO" << endl << endl;
-
+    int min = (floor(amount/coin_value[index])) > max_coin ? max_coin : floor(amount/coin_value[index]);
+    return min;
 }
 
-int main (void) {
-    int cases, amount, coins;
-    int coin_value[5];
-    int coin_used[5];
+bool amount_possible (int index, int max_coin, int amount) {
+    if (amount % coin_value[index] == 0) {
+        if ((int) amount / coin_value[index] <= max_coin) {
+            return true;
+        }
+    }
+    if (index == coins-1)
+        return false;
 
+    /* Try with every number of current coin possible */
+    for (int i = 1; i <= coin_max(index, max_coin, amount); i++) {
+        if (amount_possible(index + 1, i, amount - i * coin_value[index]))
+            return true;
+    }
+
+    return false;
+}
+
+
+int main (void) {
     cin >> cases;
     for (int n = 0; n < cases; n++) {
         cin >> amount >> coins;
@@ -40,8 +40,15 @@ int main (void) {
             cin >> coin_value[i];
         }
 
-        adem_algo(amount, coin_value, coins);
+        if (n > 0)
+            cout << endl << endl;
+        if (amount_possible(0, floor(amount/coin_value[0]), amount))
+            cout << "YES";
+        else
+            cout << "NO";
     }
+
+    cout << endl;
 
     return 0;
 }
