71ca855835497809c493ea3e1629580afb5f19cf
Mickaël Seznec <mickael.seznec@gmail.com>
2015-05-26 23:39:11 +0200

    Add new headers in blank project

--- blank_project/blank.cpp
+++ blank_project/blank.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <map>
+#include <string>
+#include <array>
+#include <queue>
+#include <utility>
+#include <set>
 
 using namespace std;
 
