46216fb7d7ff0a2886190e47ebf6a180fbcf78c0
Mickaël Seznec <mickael.seznec@gmail.com>
2015-06-05 10:16:12 +0200

    Typo : now add generator

---
+++ week_4/1138_Roof/generator.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+
+using namespace std;
+
+#define MAX_ROOFS   20000
+
+int main (void) {
+    srand (time(NULL));
+
+    int cases = rand() % 30 + 1;
+
+    while (cases --) {
+        int roofs = rand() % MAX_ROOFS + 1;
+        printf("%d\n", roofs);
+
+        for (int i = 0; i < roofs; i++) {
+            int x1 = rand() % 200 + 1; 
+            int y1 = rand() % 200 + 1; 
+            int x2 = rand() % 200 + 1; 
+            int y2 = rand() % 200 + 1; 
+
+            printf("%d %d %d %d\n", x1, y1, x2, y2);
+        }
+    }
+}
